300f394f86a9e8a294fc09ecbbdcf681d9b75088
Dmytro Karpovych <ZAYEC77@gmail.com>
2018-04-22 23:20:07 +0300

    #4 fix

--- src/components/BlogStatusList.php
+++ src/components/BlogStatusList.php
@@ -2,7 +2,7 @@
 
 namespace nullref\blog\components;
 
-use nullref\core\interfaces\IStatusList;
+use nullref\core\interfaces\IList;
 use Yii;
 use yii\base\BaseObject;
 
@@ -10,7 +10,7 @@ use yii\base\BaseObject;
  * @author    Dmytro Karpovych
  * @copyright 2015 NRE
  */
-class BlogStatusList extends BaseObject implements IStatusList
+class BlogStatusList extends BaseObject implements IList
 {
     const STATUS_DRAFT = 0;
     const STATUS_PUBLISHED = 1;
@@ -21,5 +21,10 @@ class BlogStatusList extends BaseObject implements IStatusList
             self::STATUS_DRAFT => Yii::t('blog', 'Draft'),
             self::STATUS_PUBLISHED => Yii::t('blog', 'Published')
         ];
+    }
+
+    public function getValue($key)
+    {
+        return $this->getList()[$key] ?? null;
     }
 }
